2b369d7706c31a35884decf730de962c9a65d88b
MarcosPrysthon <mvpn@cin.ufpe.br>
2020-11-15 23:32:16 -0300

    [FINISHES aiorosjr#25]Finalizando teste de aceitacao do cenario de atribuir pneu ao carro

--- tests-acceptance/features/veiculo-elemento.feature
+++ tests-acceptance/features/veiculo-elemento.feature
@@ -2,6 +2,20 @@ Feature: As an employer
     I want to see a vehicle details
 
 Scenario: Delete a vehicle
-Given I'm in page of vehicle with plate "PDY1234"
-When I click on the button "Deletar"
+Given I'm in page of vehicle with license plate "PDY1234"
+When I click on the button to delete "Deletar"
 Then I can see an alert "Veiculo deletado com sucesso"
+
+Scenario: Assign a tire to a vehicle
+Given I'm in page of vehicle with plate "PDY2345"
+When I click on the button to assign "+"
+And I enter the tire's ID "1"
+And Press the button to assign "Atribuir"
+Then I can see the tire's ID on the page of vehicle with plate "PDY2345"
+
+Scenario: Unassign a tire from a vehicle
+Given I'm in page of vehicle with plate "PDY2345"
+When I click the button to unassign "-"
+And I enter the tire's ID "2"
+And Press the button to unassing "Desatribuir"
+Then I cannot see the tire's ID on the page of vehicle with plate "PDY2345"
--- tests-acceptance/stepdefinitions/veiculo-elemento.ts
+++ tests-acceptance/stepdefinitions/veiculo-elemento.ts
@@ -5,11 +5,11 @@ let chai = require('chai').use(require('chai-as-promised'));
 let expect = chai.expect;
 
 defineSupportCode(function ({Given, When, Then}) {
-    Given(/^I'm in page of vehicle with plate "([^\"]*)"$/, async (plate) => {
+    Given(/^I'm in page of vehicle with license plate "([^\"]*)"$/, async (plate) => {
         await browser.get("http://localhost:4200/veiculos/" + plate)
     })
 
-    When(/^I click on the button "([^\"]*)"$/, async(textBtn) => {
+    When(/^I click on the button to delete "([^\"]*)"$/, async(textBtn) => {
         await element.all(by.name('deletar-btn')).click();
     })
 
@@ -20,6 +20,28 @@ defineSupportCode(function ({Given, When, Then}) {
         (await browser.switchTo().alert()).accept().catch(function(ex){
 
         });
+    })
+
+
+    Given(/^I'm in page of vehicle with plate "([^\"]*)"$/, async (plate) => {
+        await browser.get("http://localhost:4200/veiculos/" + plate);
+    })
+
+    When(/^I click on the button to assign "([^\"]*)"$/, async (btn) => {
+        await element.all(by.name('atribuir-dialog-btn')).click();
+    })
+
+    When(/^I enter the tire's ID "([^\"]*)"$/, async (tireID) => {
+        var id: string = tireID.toString();
+        await element.all(by.name('atribuir-input')).sendKeys(id);
+    })
+
+    When(/^Press the button to assign "([^\"]*)"$/, async (btn) => {
+        await element.all(by.name('atribuir-btn')).click();
+    })
+
+    Then(/^I can see the tire's ID on the page of vehicle with plate "([^\"]*)"$/, async (plate) => {
+        await browser.get("http://localhost:4200/veiculos/" + plate);
     })
 
 })
